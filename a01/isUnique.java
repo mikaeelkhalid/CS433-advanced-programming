1aa3b3cfbd738ff1bcfd15fc562c898a137e33ae
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-04-04 14:40:03 +0500

    a01 done

---
+++ a01/isUnique.java
@@ -0,0 +1,25 @@
+package a01;
+
+public class isUnique {
+
+    public static boolean isUniqueChars(String str) {
+        if (str.length() > 26) {
+            return false;
+        }
+        int checker = 0;
+        for (int i = 0; i < str.length(); i++) {
+            int val = str.charAt(i) - 'a';
+            if ((checker & (1 << val)) > 0)
+                return false;
+            checker |= (1 << val);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] words = { "abcde", "hello", "car", "city", "eminem" };
+        for (String word : words) {
+            System.out.println(word + ": " + isUniqueChars(word));
+        }
+    }
+}
